eeca2518755201fc60f5c6e681d1a6e8d65b41e6
edsmartin7 <edsmartin7@gmail.com>
2017-10-24 01:09:06 -0400

    third commit

--- app/src/main/java/com/example/emartin/layoutexamples/MainActivity.java
+++ app/src/main/java/com/example/emartin/layoutexamples/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.emartin.layoutexamples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Just an example for layouts
 public class MainActivity extends AppCompatActivity {
 
     @Override
